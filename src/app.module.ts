7ccac3ec348f3266b29c4f195de3132bb0c49ac8
JeffreyLimZL <jlzl011023@gmail.com>
2026-02-24 15:04:22 +0800

    接入 MongoDB 云端金库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // 引入金库连接线
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 把你的“终极钥匙”粘贴在下面的引号里！
+    MongooseModule.forRoot('mongodb+srv://jlzl011023_db_user:aojoxPusPI0qKke5@cluster0.v0djua2.mongodb.net/?appName=Cluster0'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
